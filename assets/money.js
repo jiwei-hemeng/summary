6851d02447603ec6be7f2eaae9b1370f4c506543
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2022-07-19 16:02:18 +0800

    Update money.js

--- assets/money.js
+++ assets/money.js
@@ -110,3 +110,18 @@ export function numberPutCommaShow(value) {
 export function ThousandNum(num) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+function formatNumber(value) {
+  value += '';
+  const list = value.split('.');
+  const prefix = list[0].charAt(0) === '-' ? '-' : '';
+  let num = prefix ? list[0].slice(1) : list[0];
+  let result = '';
+  while (num.length > 3) {
+    result = `,${num.slice(-3)}${result}`;
+    num = num.slice(0, num.length - 3);
+  }
+  if (num) {
+    result = num + result;
+  }
+  return `${prefix}${result}${list[1] ? `.${list[1]}` : ''}`;
+}
