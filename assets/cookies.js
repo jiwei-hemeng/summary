b5d21c939cd654d48cda7d48b25d16c976b8651c
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2022-09-08 18:21:01 +0800

    Create cookies.js

---
+++ assets/cookies.js
@@ -0,0 +1,24 @@
+// 设置cookie
+export function setCookie(key, value, expire) {
+ const d = new Date();
+ d.setDate(d.getDate() + expire);
+ document.cookie = `${key}=${value};expires=${d.toUTCString()}`;
+};
+// 获取 cookie
+export function getCookie(key) {
+ const cookieStr = unescape(document.cookie);
+ const arr = cookieStr.split("; ");
+ let cookieValue = "";
+ for (let i = 0; i < arr.length; i++) {
+   const temp = arr[i].split("=");
+   if (temp[0] === key) {
+     cookieValue = temp[1];
+     break;
+   }
+ }
+ return cookieValue;
+};
+// 删除cookie
+export function delCookie(key) {
+ document.cookie = `${encodeURIComponent(key)}=;expires=${new Date()}`;
+};
