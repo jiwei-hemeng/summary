dd811621f0d96482e52f4fd39e20fa8b43624f2b
jiwei-hemeng <jiwei7065@sina.com>
2022-09-11 18:53:28 +0800

    nodejs 使用rsa加密技术

--- assets/nodeRsa.js
+++ assets/nodeRsa.js
@@ -1,21 +1,16 @@
 const NodeRSA = require("node-rsa");
 const key = new NodeRSA({ b: 512 });
 key.setOptions({ encryptionScheme: "pkcs1" });
-// 私钥
-const privatePem = key.exportKey("pkcs1-private-pem");
-// 公钥
-const publicPem = key.exportKey("pkcs1-public-pem");
-console.log("=================私钥:===============")
-console.log(privatePem)
-console.log("=================公钥================")
-console.log(publicPem)
+// 私钥 - 解密
+const privatePem = key.exportKey("pkcs8-private-pem");
+// 公钥 - 加密
+const publicPem = key.exportKey("pkcs8-public-pem");
 const data = "我是加密前的数据";
-const encrypt = new NodeRSA(privatePem)
+key.importKey(publicPem)
 console.log("正在加密中...")
-const cioherText = encrypt.encryptPrivate(data, "base64");
+const cioherText = key.encrypt(data, "base64");
 console.log("加密后是：", cioherText);
-console.log("加密后长度：", cioherText.length);
-const decrypt = new NodeRSA(publicPem);
+key.importKey(privatePem);
 console.log("正在解密中...")
-const rowText = decrypt.decryptPublic(cioherText, "utf8");
+const rowText = key.decrypt(cioherText, "utf8");
 console.log("解密后是：", rowText);
