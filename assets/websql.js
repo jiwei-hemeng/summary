79e52b83becdf5378d001866409a7b27a055f02e
jiwei <62381906+jiwei-hemeng@users.noreply.github.com>
2022-11-30 18:25:45 +0800

    Update websql.js

--- assets/websql.js
+++ assets/websql.js
@@ -1,20 +1,18 @@
-function websql() {
-  const db = openDatabase("mywebdbs", "1.0", "Test DB", 2 * 1024 * 1024);
-  return function(sql, params = null) {
-    return new Promise((reslove) => {
-      db.transaction(function (dbtest) {
-        dbtest.executeSql(sql, params, (err, result) => {
-          reslove(result.rows);
-        });
+function websql(sql, params = null) {
+  return new Promise((reslove) => {
+    const db = openDatabase("mywebdbs", "1.0", "Test DB", 2 * 1024 * 1024);
+    db.transaction(function (dbtest) {
+      dbtest.executeSql(sql, params, (err, result) => {
+        reslove(result.rows);
       });
     });
-  };
+  });
 }
-const mysql = websql();
-mysql("SELECT * FROM CHART").then((result) => {
+websql("SELECT * FROM CHART").then((result) => {
   console.log("res", result);
 });
-mysql("INSERT INTO CHART (id,log) VALUES (7,'hello word7')")
-  .then((res) => {
+websql("INSERT INTO CHART (id,log) VALUES (7,'hello word7')").then(
+  (res) => {
     console.log("11111", res);
-  });
+  }
+);
