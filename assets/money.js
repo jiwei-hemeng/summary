90e585be9d70ceac1d7173be7f9fae603e99242a
jijiwei <jijiwei@lilianinfo.com>
2023-03-28 17:24:16 +0800

    moneyFormat

--- assets/money.js
+++ assets/money.js
@@ -135,3 +135,33 @@ function formatNumber(value) {
   }
   return `${prefix}${result}${list[1] ? `.${list[1]}` : ''}`;
 }
+
+/** 
+ * 将数字按照每隔3位逗号分割
+ * @param {number} number 需要转换的数字
+ * @param {number} prec 保留几位小数
+ * @param {string} dec 小数点符号
+ * @param {string} sep 千分位符号
+ * @return {string}
+ */
+
+export function moneyFormat (number, prec = 2, dec = ".", sep = ",") {
+  number = (number + '').replace(/[^0-9+-Ee.]/g, '')
+  const n = !isFinite(+number) ? 0 : +number
+  let s = ''
+  const toFixedFix = function(n, prec) {
+    const k = Math.pow(10, prec)
+    return '' + Math.ceil(n * k) / k
+  }
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.')
+  const re = /(-?\d+)(\d{3})/
+  while (re.test(s[0])) {
+    s[0] = s[0].replace(re, '$1' + sep + '$2')
+  }
+
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || ''
+    s[1] += new Array(prec - s[1].length + 1).join('0')
+  }
+  return s.join(dec)
+}
