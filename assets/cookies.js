f5be6e609acda0f74a79bab2f1f34c06aeb8b2b3
jiwei-hemeng <jiwei7065@sina.com>
2025-08-13 19:27:03 +0800

    cookie 关于 JSDOC 的注解

--- assets/cookies.js
+++ assets/cookies.js
@@ -1,10 +1,20 @@
-// 设置cookie
+/**
+ * 设置 cookie
+ * @param {string} key - Cookie的键
+ * @param {string} value - Cookie的值
+ * @param {number} expire - Cookie的过期时间（天）
+ * @return {void}
+ */
 export function setCookie(key, value, expire) {
   const d = new Date();
   d.setDate(d.getDate() + expire);
   document.cookie = `${key}=${value};expires=${d.toUTCString()}`;
-};
-// 获取 cookie
+}
+/**
+ * 获取 cookie
+ * @param {string} key - Cookie的键
+ * @returns {string} - Cookie的值
+ */
 export function getCookie(key) {
   const cookieStr = decodeURIComponent(document.cookie);
   const arr = cookieStr.split("; ");
@@ -17,8 +27,12 @@ export function getCookie(key) {
     }
   }
   return cookieValue;
-};
-// 删除cookie
+}
+/**
+ * 删除 cookie
+ * @param {string} key - Cookie的键
+ * @return {void}
+ */
 export function delCookie(key) {
   document.cookie = `${encodeURIComponent(key)}=;expires=${new Date()}`;
-};
+}
