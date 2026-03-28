ca24a8dd3849cdc43bfc571338fdc5f2da642a72
jiwei-hemeng <jiwei7065@sina.com>
2026-03-28 18:30:11 +0800

    bpmn 如何自定义属性面板

--- bpmn/src/main.js
+++ bpmn/src/main.js
@@ -37,4 +37,33 @@ const saveXML = async () => {
   return xml;
 };
 
+let currentUserTaskElement = null;
+// 选中某个节点时触发的事件
+modeler.on("selection.changed", function (e) {
+  if (
+    e.newSelection.length === 1 &&
+    e.newSelection[0].type === "bpmn:UserTask"
+  ) {
+    // 表示选中用户节点;
+    currentUserTaskElement = e.newSelection[0];
+    console.log("节点对象", currentUserTaskElement.businessObject);
+    console.log("节点名称", currentUserTaskElement.businessObject.name);
+    console.log("节点id", currentUserTaskElement.businessObject.id);
+    document.querySelector("#input").value =
+      currentUserTaskElement.businessObject.name;
+  } else {
+    currentUserTaskElement = null;
+    document.querySelector("#input").value = "";
+  }
+});
+// 更新数据
+function updateModeler(e) {
+  const modeling = modeler.get("modeling");
+  modeling.updateProperties(currentUserTaskElement, {
+    name: e.target.value,
+  });
+}
+
+document.querySelector("#input").addEventListener("input", updateModeler);
+
 document.getElementById("save").addEventListener("click", saveXML);
